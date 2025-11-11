d9d23f2c3d18219c30de9329f754c7cf5f3f7b80
T.Bicanic <t.bicanic@posteo.de>
2025-11-11 16:42:39 +0100

    Hehe

--- scripts/raw/GEMM_stride/intel/02_run_benchmark.sh
+++ scripts/raw/GEMM_stride/intel/02_run_benchmark.sh
@@ -23,7 +23,8 @@ echo ""
 
 # Compile
 echo "Compiling cpu_bench..."
-g++ -O3 -march=native -std=c++17 -Wall -Wextra -o cpu_bench main.cpp -lopenblas
+g++ -O3 -march=native -fopenmp main.cpp -lopenblas -o cpu_benchmark
+
 
 if [ $? -ne 0 ]; then
     echo "ERROR: Compilation failed!"
